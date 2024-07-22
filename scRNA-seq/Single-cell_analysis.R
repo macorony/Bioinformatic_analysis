0e79a581ebae890c5940b5122f79d633d75546fc
Gang Yan <yangangmg@gmail.com>
2024-07-22 15:45:06 -0400

    update

--- scRNA-seq/Single-cell_analysis.R
+++ scRNA-seq/Single-cell_analysis.R
@@ -130,3 +130,93 @@ colLabels(sce) <- clusterCells(sce, use.dimred = "PCA", BLUSPARAM = NNGraphParam
 sce <- runUMAP(sce, dimred = "PCA")
 plotUMAP(sce, colour_by = "label")
 
+# human pancreas single cell analysis
+# Data loading
+library(scRNAseq)
+sce.seger <- SegerstolpePancreasData()
+library(AnnotationHub)
+edb <- AnnotationHub()[["AH73881"]]
+symbols <- rowData(sce.seger)$symbol
+ens.id <- mapIds(edb, keys=symbols, keytype="SYMBOL", column="GENEID")
+ens.id <- ifelse(is.na(ens.id), symbols, ens.id)
+
+keep <- !duplicated(ens.id)
+sce.seger <- sce.seger[keep,]
+rownames(sce.seger) <- ens.id[keep]
+
+emtab.meta <- colData(sce.seger)[, c("cell type", "disease", "individual", "single cell well quality")]
+colnames(emtab.meta) <- c("CellType", "Disease", "Donor", "Quality")
+colData(sce.seger) <- emtab.meta
+
+sce.seger$CellType <- gsub(" cell", "", sce.seger$CellType)
+sce.seger$CellType <- paste0(
+  toupper(substr(sce.seger$CellType, 1, 1)),
+  substring(sce.seger$CellType, 2))
+
+# Quality control
+unfiltered <- sce.seger
+low.qual <- sce.seger$Quality == "low quality cell"
+
+library(scater)
+stats <- perCellQCMetrics(sce.seger)
+qc <- quickPerCellQC(stats, percent_subsets="altexps_ERCC_percent", batch=sce.seger$Donor, 
+                     subset=!sce.seger$Donor %in% c("HP1504901", "HP1509101"))
+sce.seger <- sce.seger[, !(qc$discard | low.qual)]
+
+colData(unfiltered) <- cbind(colData(unfiltered), stats)
+unfiltered$discard <- qc$discard
+
+gridExtra::grid.arrange(
+  plotColData(unfiltered, x="Donor", y="sum", colour_by = "discard") +
+    scale_y_log10() + ggtitle("Total count") +
+    theme(axis.text.x = element_text(angle = 90)),
+  plotColData(unfiltered, x="Donor", y="detected", colour_by = "discard") +
+    scale_y_log10() + ggtitle("Detected features") +
+    theme(axis.text.x = element_text(angle = 90)),
+  plotColData(unfiltered, x="Donor", y="altexps_ERCC_percent", colour_by = "discard") +
+    ggtitle("ERCC percent") +
+    theme(axis.text.x = element_text(angle = 90)), 
+  ncol = 2
+)
+
+# Normalization
+library(scran)
+clusters <- quickCluster(sce.seger)
+sce.seger <- computeSumFactors(sce.seger, clusters=clusters)
+sce.seger <- logNormCounts(sce.seger)
+
+summary(sizeFactors(sce.seger))
+plot(librarySizeFactors(sce.seger), sizeFactors(sce.seger), pch=16, 
+     xlab = "Library size factors", ylab = "Deconvolution factors", log="xy")
+
+# Variance modeling
+for.hvg <- sce.seger[, librarySizeFactors(altExp(sce.seger)) > 0 & sce.seger$Donor!="AZ"]
+dec.seger <- modelGeneVarWithSpikes(for.hvg, "ERCC", block=for.hvg$Donor)
+chosen.hvgs <- getTopHVGs(dec.seger, n=2000)
+
+par(mfrow=c(3,3))
+blocked.stats <- dec.seger$per.block
+for (i in colnames(blocked.stats)) {
+  current <- blocked.stats[[i]]
+  plot(current$mean, current$total, main=i, pch=16, cex=0.5, 
+       xlab="Mean of log-expression", ylab="Variance of log-expression")
+  curfit <- metadata(current)
+  points(curfit$mean, curfit$var, col="red", pch=16)
+  curve(curfit$trend(x), col="dodgerblue", add=T, lwd=2)
+}
+
+# Dimensionality reduction
+library(BiocSingular)
+set.seed(100)
+sce.seger <- runPCA(sce.seger, subset_row=chosen.hvgs, ncomponents=25)
+sce.seger <- runTSNE(sce.seger, dimred="PCA")
+
+# Clustering
+library(bluster)
+clust.out <- clusterRows(reducedDim(sce.seger, "PCA"), NNGraphParam(), full = T)
+snn.gr <- clust.out$objects$graph
+colLabels(sce.seger) <- clust.out$clusters
+
+
+
+
